8f4b58daf3fa055d61332733e03648116ba57a3c
Anbu <15264938+anburocky3@users.noreply.github.com>
2022-10-12 22:04:51 +0530

    feat: Added javascript interactions

--- main.js
+++ main.js
@@ -7,61 +7,67 @@ const finalResult = document.querySelector('#result');
 const allNumbers = document.querySelectorAll('[data-operand]');
 const allOperands = document.querySelectorAll('[data-operation]');
 
-let currentOperator = '';
-let firstOperand = '';
-
 const appendText = (numbers) => {
   operand.innerText += numbers;
 };
 
-const setNumberClickListeners = (numEl) => {
-  numEl.addEventListener('click', () => {
-    appendText(parseInt(numEl.innerText));
-  });
+const getOnlyNumbersAndOperands = (e) => {
+  if (
+    (e.which >= 48 && e.which <= 57) ||
+    (e.keyCode >= 96 && e.keyCode <= 111) ||
+    e.keyCode === 13
+  ) {
+    if (e.keyCode === 13) evaluate();
+    else appendText(e.key);
+  } else {
+    return false;
+  }
 };
 
-const setOperations = (operator) => {
-  //   console.log(parseInt(finalResult.textContent));
-  //   if (parseInt(finalResult.textContent) !== 0) {
-  //     console.log('!=0');
-  //     currentOperator = operator;
-  //     firstOperand = parseInt(finalResult.textContent);
-  //     appendText(operator);
-  //     const res = operate(operator, firstOperand, 2);
-  //     finalResult.innerText = res;
-  //   }
+const appendTextFunc = (e) => {
+  if (e.type === 'click') {
+    appendText(e.target.innerText);
+  } else if (e.type === 'keydown') {
+    getOnlyNumbersAndOperands(e);
+  }
+};
+
+const setNumberClickListeners = (numEl) => {
+  numEl.addEventListener('click', appendTextFunc);
 };
 
 allNumbers.forEach((numEl) => setNumberClickListeners(numEl));
 
 allOperands.forEach((operationEl) => {
-  operationEl.addEventListener('click', () =>
-    // setOperations(operationEl.innerText)
-    appendText(operationEl.innerText)
-  );
+  operationEl.addEventListener('click', appendTextFunc);
 });
 
 equalOperator.addEventListener('click', () => {
-  const result = eval(operand.innerText);
-  finalResult.innerText = result;
+  evaluate();
 });
 
-clearOperandEl.addEventListener('click', () => {
+const clearData = (e) => {
   operand.innerText = '';
   finalResult.innerText = '0';
+};
+
+clearOperandEl.addEventListener('click', clearData);
+
+document.body.addEventListener('keydown', appendTextFunc);
+document.body.addEventListener('keydown', (e) => {
+  if (e.key === 'Delete') {
+    clearData();
+  }
 });
 
-function operate(operator, a, b) {
-  switch (operator) {
-    case '+':
-      return a + b;
-    case '-':
-      return a - b;
-    case '/':
-      return a / b;
-    case '*':
-      return a * b;
-    default:
-      return 'Not a number';
+document.body.addEventListener('keydown', (e) => {
+  if (e.key === 'Backspace') {
+    const sliced = operand.innerText.slice(0, -1);
+    operand.innerText = sliced;
   }
+});
+
+function evaluate() {
+  const result = eval(operand.innerText);
+  finalResult.innerText = result;
 }
